43ce7be24d6720e3e8d42505799593bf519af285
djonatas <djonatas.tenfen@gmail.com>
2015-05-14 17:33:25 -0300

    inclusao do arquivo de banco de dados

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
+require('./config/database.js')('mongodb://localhost/APITest');
+
 
 http.createServer(app).listen(app.get('port'), function (){
     console.log('Tudo funcionando :) -  Porta:'+ app.get('port'));
