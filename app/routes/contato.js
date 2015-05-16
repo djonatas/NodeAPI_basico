73ecc8c1f9b939e7230942257d59013aea9e4db5
djonatas <djonatas.tenfen@gmail.com>
2015-05-15 23:27:29 -0300

    implementacao de route para ficar mais clarro o arquivo de rotas e criacao dos verbos post e delete

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -2,6 +2,11 @@ module.exports = function(app) {
 
     var controller = app.controllers.contato;
 
-    app.get('/contatos', controller.listaContatos);
-    app.get('/contatos/:id', controller.obtemContato);
+    app.route('/contatos')
+        .get(controller.listaContatos)
+        .post(controller.salvaContato);
+
+    app.route('/contato/:id')
+        .get(controller.obtemContato)
+        .delete(controller.removeContato);
 };
