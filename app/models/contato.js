07c8ec9adf7cbaeade5aeb9e8749e74e1952f4ae
djonatas <djonatas.tenfen@gmail.com>
2015-05-15 23:37:28 -0300

    Alteracoes dos modelo

--- app/models/contato.js
+++ app/models/contato.js
@@ -31,7 +31,6 @@ module.exports = function() {
             type: String,
             required: true,
             match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Por favor informe um email valido'],
-            validate: [validateUniqueEmail, 'E-mail address is already in-use'],
             index: {
                 unique: true
             }
@@ -53,68 +52,33 @@ module.exports = function() {
         linkedin: {}
     });
 
+
+
+    // rotinas que podem ser útil algum dia
     UserSchema.methods = {
-        /**
-         * HasRole - check if the user has required role
-         *
-         * @param {String} plainText
-         * @return {Boolean}
-         * @api public
-         */
         hasRole: function(role) {
             var roles = this.roles;
             return roles.indexOf('admin') !== -1 || roles.indexOf(role) !== -1;
         },
 
-        /**
-         * IsAdmin - check if the user is an administrator
-         *
-         * @return {Boolean}
-         * @api public
-         */
         isAdmin: function() {
             return this.roles.indexOf('admin') !== -1;
         },
 
-        /**
-         * Authenticate - check if the passwords are the same
-         *
-         * @param {String} plainText
-         * @return {Boolean}
-         * @api public
-         */
         authenticate: function(plainText) {
             return this.hashPassword(plainText) === this.hashed_password;
         },
 
-        /**
-         * Make salt
-         *
-         * @return {String}
-         * @api public
-         */
         makeSalt: function() {
             return crypto.randomBytes(16).toString('base64');
         },
 
-        /**
-         * Hash password
-         *
-         * @param {String} password
-         * @return {String}
-         * @api public
-         */
         hashPassword: function(password) {
             if (!password || !this.salt) return '';
             var salt = new Buffer(this.salt, 'base64');
             return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
         },
 
-        /**
-         * Hide security sensitive fields
-         *
-         * @returns {*|Array|Binary|Object}
-         */
         toJSON: function() {
             var obj = this.toObject();
             delete obj.hashed_password;
